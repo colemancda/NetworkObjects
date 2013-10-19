019622e70cc0cc1f367dc340151b5cfcbe4555ed
Alsey Coleman Miller <alseycmiller@gmail.com>
2013-10-19 17:21:30 -0400

    gonna use NSIncrementalStore for iOS client store

---
+++ NOClientExample/NOClientExample/NOClientIncrementalStore.h
@@ -0,0 +1,13 @@
+//
+//  NOClientIncrementalStore.h
+//  NOClientExample
+//
+//  Created by Alsey Coleman Miller on 10/19/13.
+//  Copyright (c) 2013 CDA. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NOClientIncrementalStore : NSIncrementalStore
+
+@end
---
+++ NOClientExample/NOClientExample/NOClientIncrementalStore.m
@@ -0,0 +1,13 @@
+//
+//  NOClientIncrementalStore.m
+//  NOClientExample
+//
+//  Created by Alsey Coleman Miller on 10/19/13.
+//  Copyright (c) 2013 CDA. All rights reserved.
+//
+
+#import "NOClientIncrementalStore.h"
+
+@implementation NOClientIncrementalStore
+
+@end
