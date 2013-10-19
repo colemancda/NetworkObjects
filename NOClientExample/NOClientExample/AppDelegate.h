701fb18680eee8c37458762ebdf41cfa6527d920
Alsey Coleman Miller <alseycmiller@gmail.com>
2013-10-19 17:06:21 -0400

    adding example project

---
+++ NOClientExample/NOClientExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NOClientExample
+//
+//  Created by Alsey Coleman Miller on 10/19/13.
+//  Copyright (c) 2013 CDA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NOClientExample/NOClientExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NOClientExample
+//
+//  Created by Alsey Coleman Miller on 10/19/13.
+//  Copyright (c) 2013 CDA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
