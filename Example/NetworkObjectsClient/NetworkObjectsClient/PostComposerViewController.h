06724ffd293118b0c16d19d522c253e47364fdcc
Alsey Coleman Miller <alseycmiller@gmail.com>
2013-11-11 14:52:39 -0500

    added PostComposerVC

---
+++ Example/NetworkObjectsClient/NetworkObjectsClient/PostComposerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostComposerViewController.h
+//  NetworkObjectsClient
+//
+//  Created by Alsey Coleman Miller on 11/11/13.
+//  Copyright (c) 2013 CDA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostComposerViewController : UIViewController
+
+@end
---
+++ Example/NetworkObjectsClient/NetworkObjectsClient/PostComposerViewController.m
@@ -0,0 +1,38 @@
+//
+//  PostComposerViewController.m
+//  NetworkObjectsClient
+//
+//  Created by Alsey Coleman Miller on 11/11/13.
+//  Copyright (c) 2013 CDA. All rights reserved.
+//
+
+#import "PostComposerViewController.h"
+
+@interface PostComposerViewController ()
+
+@end
+
+@implementation PostComposerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
