cbb30b51afb8859688ca73fb3ca317e136180eb9
Alsey Coleman Miller <alseycmiller@gmail.com>
2013-12-30 17:07:50 -0500

    adding documentation to NOResourceProtocol constants

--- NetworkObjects/NOResourceProtocolConstants.h
+++ NetworkObjects/NOResourceProtocolConstants.h
@@ -9,19 +9,38 @@
 #ifndef NetworkObjects_NOResourceProtocolConstants_h
 #define NetworkObjects_NOResourceProtocolConstants_h
 
+/**
+ Permission constants for Resources
+ */
+
 typedef NS_ENUM(NSUInteger, NOResourcePermission) {
     
+    /**  No access permission */
     NoAccessPermission = 0,
+    
+    /**  Read Only permission */
     ReadOnlyPermission = 1,
+    
+    /**  Read and Write permission */
     EditPermission
     
 };
 
+/**
+ Resource Function constants
+ */
 typedef NS_ENUM(NSUInteger, NOResourceFunctionCode) {
     
+    /** The function performed successfully */
     FunctionPerformedSuccesfully = 200,
+    
+    /** The function recieved an invalid JSON object */
     FunctionRecievedInvalidJSONObject = 400,
+    
+    /** The function cannot be performed, possibly due to session permissions */
     CannotPerformFunction = 403,
+    
+    // ** There was an internal error while performing the function */
     InternalErrorPerformingFunction = 500
 };
 
