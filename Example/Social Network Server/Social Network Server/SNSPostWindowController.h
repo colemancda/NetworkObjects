f3488850f1f94f9bc4bdc368390db8e89d0dc91e
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-02-16 01:30:02 -0500

    more cocoa bindings

---
+++ Example/Social Network Server/Social Network Server/SNSPostWindowController.h
@@ -0,0 +1,14 @@
+//
+//  SNSPostWindowController.h
+//  Social Network Server
+//
+//  Created by Alsey Coleman Miller on 2/16/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "SNSRepresentedObjectWindowController.h"
+
+@interface SNSPostWindowController : SNSRepresentedObjectWindowController
+
+@end
---
+++ Example/Social Network Server/Social Network Server/SNSPostWindowController.m
@@ -0,0 +1,33 @@
+//
+//  SNSPostWindowController.m
+//  Social Network Server
+//
+//  Created by Alsey Coleman Miller on 2/16/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import "SNSPostWindowController.h"
+
+@interface SNSPostWindowController ()
+
+@end
+
+@implementation SNSPostWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
