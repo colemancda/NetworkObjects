3472fa9777b3f681ac0d6f0b517d67d91a2fe306
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-02-21 22:11:09 -0500

    added storyboard

---
+++ Example/Social Network Client/Social Network Client/SNCStore.h
@@ -0,0 +1,18 @@
+//
+//  SNCStore.h
+//  Social Network Client
+//
+//  Created by Alsey Coleman Miller on 2/21/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <NetworkObjects/NetworkObjects.h>
+
+@interface SNCStore : NSObject
+
++ (instancetype)sharedStore;
+
+
+
+@end
---
+++ Example/Social Network Client/Social Network Client/SNCStore.m
@@ -0,0 +1,38 @@
+//
+//  SNCStore.m
+//  Social Network Client
+//
+//  Created by Alsey Coleman Miller on 2/21/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import "SNCStore.h"
+
+@implementation SNCStore
+
++ (instancetype)sharedStore
+{
+    static SNCStore *sharedStore = nil;
+    if (!sharedStore) {
+        sharedStore = [[super allocWithZone:nil] init];
+    }
+    return sharedStore;
+}
+
++ (id)allocWithZone:(NSZone *)zone
+{
+    return [self sharedStore];
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        
+        
+        
+    }
+    return self;
+}
+
+@end
