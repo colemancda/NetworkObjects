49aaa87e1c103adef446e6b63657e3be260bf8ce
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-04-12 18:37:59 -0400

    NOAPICachedStore deletes resource if not found
    when fetching a resource with GET, if the server returns a not found
    error, then NOAPICachedStore will delete the local copy

--- NetworkObjects/NOAPICachedStore.m
+++ NetworkObjects/NOAPICachedStore.m
@@ -216,6 +216,45 @@ NSString *const NOAPICachedStoreDatesCachedOption = @"NOAPICachedStoreDatesCache
         
         if (error) {
             
+            // not found, delete object from our cache
+            
+            if (error.code == NOAPINotFoundErrorCode) {
+                
+                // get resourceID
+                
+                Class entityClass = NSClassFromString([self.context.persistentStoreCoordinator.managedObjectModel.entitiesByName[resourceName] managedObjectClassName]);
+                
+                NSString *resourceIDKey = [entityClass resourceIDKey];
+                
+                NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:resourceName];
+                
+                fetchRequest.predicate = [NSPredicate predicateWithFormat:@"%K == %lu", resourceIDKey, resourceID];
+                
+                [_context performBlockAndWait:^{
+                    
+                    NSError *fetchError;
+                    
+                    NSArray *results = [_context executeFetchRequest:fetchRequest
+                                                               error:&fetchError];
+                    
+                    NSManagedObject *resource = results.firstObject;
+                    
+                    // delete resouce of there is one in cache
+                    
+                    if (resource) {
+                        
+                        [_context deleteObject:resource];
+                        
+                        // optionally process pending changes
+                        
+                        if (self.shouldProcessPendingChanges) {
+                            
+                            [self.context processPendingChanges];
+                        }
+                    }
+                }];
+            }
+            
             completionBlock(error, nil);
             
             return;
