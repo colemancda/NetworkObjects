7fc4b8969020fc29861b1b561d096b8a0d033ecc
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-06-13 19:41:14 -0400

    added NOStore and iOS target

---
+++ NetworkObjects/NetworkObjects/NOStore.m
@@ -0,0 +1,13 @@
+//
+//  NOStore.m
+//  NetworkObjects
+//
+//  Created by Alsey Coleman Miller on 6/13/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import "NOStore.h"
+
+@implementation NOStore
+
+@end
