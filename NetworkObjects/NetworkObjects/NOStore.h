e22252c31252882547e12d7bd3639fdf95b2ef84
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-06-13 19:54:45 -0400

    added NOStore class

---
+++ NetworkObjects/NetworkObjects/NOStore.h
@@ -0,0 +1,13 @@
+//
+//  NOStore.h
+//  NetworkObjects
+//
+//  Created by Alsey Coleman Miller on 6/13/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NOStore : NSObject
+
+@end
