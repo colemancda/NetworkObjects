030ba511a249190bc844eea96881b70dac05e5e9
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-09-19 21:14:50 -0400

    working on Store search request

--- NetworkObjects/NetworkObjects/Store.swift
+++ NetworkObjects/NetworkObjects/Store.swift
@@ -41,6 +41,9 @@ public class Store {
     /** The managed object context running on a background thread for asyncronous caching. */
     private let privateQueueManagedObjectContext: NSManagedObjectContext = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.PrivateQueueConcurrencyType)
     
+    /** A convenience variable for the managed object model. */
+    private let model: NSManagedObjectModel
+    
     // MARK: - Initialization
     
     deinit {
@@ -73,6 +76,7 @@ public class Store {
         self.managedObjectContext = NSManagedObjectContext(concurrencyType: managedObjectContextConcurrencyType)
         self.managedObjectContext.undoManager = nil
         self.managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+        self.model = persistentStoreCoordinator.managedObjectModel
         
         self.privateQueueManagedObjectContext.undoManager = nil
         self.privateQueueManagedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
@@ -83,7 +87,67 @@ public class Store {
     
     // MARK: - Requests
     
-    
+    /** Performs a search request on the server. The supplied fetch request's predicate must be a NSComparisonPredicate. */
+    public func performSearch(fetchRequest: NSFetchRequest, URLSession: NSURLSession, completionBlock: ((error: NSError?, results: [NSManagedObject]?) -> Void)) -> NSURLSessionDataTask {
+        
+        // build JSON request from fetch request
+        
+        var jsonObject = [String: AnyObject]()
+        
+        // optional comparison predicate
+        
+        let predicate = fetchRequest.predicate as? NSComparisonPredicate
+        
+        if predicate != nil && predicate?.predicateOperatorType != NSPredicateOperatorType.CustomSelectorPredicateOperatorType {
+            
+            jsonObject[SearchParameter.PredicateKey.toRaw()] = predicate?.leftExpression.keyPath
+            
+            // convert from Core Data to JSON
+            let jsonValue: AnyObject? = fetchRequest.entity.JSONObjectFromCoreDataValues([predicate!.leftExpression.keyPath: predicate!.rightExpression.constantValue]).values.first
+            
+            jsonObject[SearchParameter.PredicateValue.toRaw()] = jsonValue
+            
+            jsonObject[SearchParameter.PredicateOperator.toRaw()] = predicate?.predicateOperatorType.toRaw()
+            
+            jsonObject[SearchParameter.PredicateOption.toRaw()] = predicate?.options.toRaw()
+            
+            jsonObject[SearchParameter.PredicateModifier.toRaw()] = predicate?.comparisonPredicateModifier.toRaw()
+        }
+        
+        // other fetch parameters
+        
+        if fetchRequest.fetchLimit != 0 {
+            jsonObject[SearchParameter.FetchLimit.toRaw()] = fetchRequest.fetchLimit
+        }
+        
+        if fetchRequest.fetchOffset != 0 {
+            jsonObject[SearchParameter.FetchOffset.toRaw()] = fetchRequest.fetchOffset
+        }
+        
+        jsonObject[SearchParameter.IncludesSubentities.toRaw()] = fetchRequest.includesSubentities
+        
+        // sort descriptors
+        
+        if fetchRequest.sortDescriptors.count != 0 {
+            
+            var jsonSortDescriptors = [[String: AnyObject]]()
+            
+            for sort in fetchRequest.sortDescriptors as [NSSortDescriptor] {
+                
+                jsonSortDescriptors.append([sort.key!: sort.ascending])
+            }
+            
+            jsonObject[SearchParameter.SortDescriptors.toRaw()] = jsonSortDescriptors
+        }
+        
+        // get entity
+        
+        let entity = self.model.entity
+        
+        
+        
+        return NSURLSessionDataTask()
+    }
     
     // MARK: - Internal Methods
     
@@ -112,9 +176,25 @@ public class Store {
     
     // MARK: Convert
     
+    
+    
     // MARK: Errors
     
     // MARK: Cache
     
+    
+    
+}
+
+// MARK: - Extensions
+
+private extension NSEntityDescription {
+    
+    func JSONObjectFromCoreDataValues(values: [String: AnyObject]) -> [String: AnyObject] {
+        
+        var jsonObject = [String: AnyObject]()
+        
+        return jsonObject
+    }
 }
 
