colemancda/NetworkObjects

commit d4ef30ed7ca1209607e399de9a3a3a7d1052ff26

named private managed object context in Store
self.model is now a copy of the model provided at initialization

NetworkObjects/NetworkObjects/Store.swift
@@ -61,7 +61,7 @@ public class Store {
             self.prettyPrintJSON = prettyPrintJSON
             self.resourceIDAttributeName = resourceIDAttributeName
             self.dateCachedAttributeName = dateCachedAttributeName
-            self.managedObjectModel = managedObjectModel
+            self.managedObjectModel = managedObjectModel.copy() as NSManagedObjectModel
             
             // edit model
             
@@ -81,6 +81,7 @@ public class Store {
             
             self.privateQueueManagedObjectContext.undoManager = nil
             self.privateQueueManagedObjectContext.persistentStoreCoordinator = self.managedObjectContext.persistentStoreCoordinator
+            self.privateQueueManagedObjectContext.name = "NetworkObjects.Store Private Managed Object Context"
             
             // listen for notifications (for merging changes)
             NSNotificationCenter.defaultCenter().addObserver(self, selector: "mergeChangesFromContextDidSaveNotification:", name: NSManagedObjectContextDidSaveNotification, object: self.privateQueueManagedObjectContext)

commit dfaffa6c8f28fd849d180ca44c2e7d5cef4e5474

fixed managed object model inconsistencies
caused by d4ef30ed7ca1209607e399de9a3a3a7d1052ff26

NetworkObjects/NetworkObjects/Store.swift
@@ -77,7 +77,7 @@ public class Store {
             // setup managed object contexts
             self.managedObjectContext = NSManagedObjectContext(concurrencyType: managedObjectContextConcurrencyType)
             self.managedObjectContext.undoManager = nil
-            self.managedObjectContext.persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+            self.managedObjectContext.persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
             
             self.privateQueueManagedObjectContext.undoManager = nil
             self.privateQueueManagedObjectContext.persistentStoreCoordinator = self.managedObjectContext.persistentStoreCoordinator