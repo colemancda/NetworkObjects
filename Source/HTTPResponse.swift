a44b206b1dec85ebcdc650592fe19397725d55fe
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-09-10 17:22:38 -0500

    working on HTTP response

---
+++ Source/HTTPResponse.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPResponse.swift
+//  NetworkObjects
+//
+//  Created by Alsey Coleman Miller on 9/9/15.
+//  Copyright © 2015 ColemanCDA. All rights reserved.
+//
+
+import SwiftFoundation
+import CoreModel
+
+public extension ResponseMessage {
+    
+    
+}
