edcb5e6e3f42cd619c91757ab2c02ae2ff41df9f
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-09-11 01:57:50 -0500

    implemented HTTP response parsing

--- Source/HTTPResponse.swift
+++ Source/HTTPResponse.swift
@@ -11,5 +11,112 @@ import CoreModel
 
 public extension ResponseMessage {
     
-    
+    /// Decode from HTTP Response.
+    init?(HTTPResponse: SwiftFoundation.HTTP.Response, parameters: (type: RequestType, entity: Entity)) {
+        
+        let requestType = parameters.type
+        
+        let entity = parameters.entity
+        
+        self.metadata = HTTPResponse.headers
+        
+        // check for error code
+        
+        guard HTTPResponse.statusCode == HTTP.StatusCode.OK.rawValue else {
+            
+            self.response = Response.Error(HTTPResponse.statusCode)
+            
+            return
+        }
+        
+        // parse response body
+        
+        let jsonResponse: JSON.Value?
+        
+        do {
+            
+            let data = HTTPResponse.body
+            
+            if data.count > 0 {
+                
+                var jsonString = ""
+                
+                for byte in data {
+                    
+                    let unicode = UnicodeScalar(byte)
+                    
+                    jsonString.append(unicode)
+                }
+                
+                guard let jsonValue = JSON.Value(string: jsonString) else { return nil }
+                
+                jsonResponse = jsonValue
+            }
+                
+            else { jsonResponse = nil }
+        }
+        
+        // parse request
+        
+        switch requestType {
+            
+        case .Get:
+            
+            guard let jsonObject = jsonResponse?.objectValue,
+                let values = entity.convert(jsonObject)
+                else { return nil }
+            
+            self.response = Response.Get(values)
+            
+        case .Delete:
+            
+            guard jsonResponse == nil else { return nil }
+            
+            self.response = Response.Delete
+            
+        case .Edit:
+            
+            guard let jsonObject = jsonResponse?.objectValue,
+                let values = entity.convert(jsonObject)
+                else { return nil }
+            
+            self.response = Response.Edit(values)
+            
+        case .Create:
+            
+            // parse response
+            guard let jsonObject = jsonResponse?.objectValue
+                where jsonObject.count == 1,
+                let (resourceID, valuesJSON) = jsonObject.first,
+                let valuesJSONObect = valuesJSON.objectValue,
+                let values = entity.convert(valuesJSONObect)
+                else { return nil }
+            
+            self.response = Response.Create(resourceID, values)
+            
+        case .Search:
+            
+            guard let resourceIDs = jsonResponse?.rawValue as? [String]
+                else { return nil }
+            
+            self.response = Response.Search(resourceIDs)
+            
+        case .Function:
+            
+            let jsonObject: JSON.Object?
+            
+            if let jsonValue = jsonResponse {
+                
+                guard let objectValue = jsonValue.objectValue else { return nil }
+                
+                jsonObject = objectValue
+            }
+            
+            else { jsonObject = nil }
+            
+            self.response = Response.Function(jsonObject)
+        }
+    }
 }
+
+
