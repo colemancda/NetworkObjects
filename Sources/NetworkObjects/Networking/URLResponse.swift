76c0b0f71796cbba767d481e9c133671f6a3ce7d
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-16 20:09:29 -0400

    Add `URLClient.response()`

---
+++ Sources/NetworkObjects/Networking/URLResponse.swift
@@ -0,0 +1,70 @@
+//
+//  URLResponse.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/16/23.
+//
+
+import Foundation
+
+internal extension URLClient {
+    
+    func response<Request, Response>(
+        _ response: Response.Type,
+        for request: Request,
+        server: URL,
+        decoder: JSONDecoder,
+        authorization authorizationToken: AuthorizationToken? = nil,
+        statusCode: Int = 200,
+        headers: [String: String] = [:]
+    ) async throws -> Response where Request: URLRequestConvertible, Response: Decodable {
+        var headers = headers
+        headers["accept"] = "application/json"
+        let data = try await self.request(
+            request,
+            server: server,
+            authorization: authorizationToken,
+            statusCode: statusCode,
+            headers: headers
+        )
+        do {
+            return try decoder.decode(Response.self, from: data)
+        } catch {
+            #if DEBUG
+            throw error
+            #else
+            throw Error.invalidResponse(data)
+            #endif
+        }
+    }
+    
+    func response<Request, Response>(
+        _ response: Response.Type,
+        for request: Request,
+        server: URL,
+        encoder: JSONEncoder,
+        decoder: JSONDecoder,
+        authorization authorizationToken: AuthorizationToken? = nil,
+        statusCode: Int = 200,
+        headers: [String: String] = [:]
+    ) async throws -> Response where Request: EncodableURLRequest, Response: Decodable {
+        var headers = headers
+        headers["accept"] = "application/json"
+        let data = try await self.request(
+            request,
+            server: server,
+            authorization: authorizationToken,
+            statusCode: statusCode,
+            headers: headers
+        )
+        do {
+            return try decoder.decode(Response.self, from: data)
+        } catch {
+            #if DEBUG
+            throw error
+            #else
+            throw Error.invalidResponse(data)
+            #endif
+        }
+    }
+}
